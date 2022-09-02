38972fe1914f7918dd988b06e41ce87333db0b30
Batuhan Aslan <pyxledev0@gmail.com>
2022-09-02 21:05:25 +0300

    fix the tabs

--- scripts/weaponDrop.lua
+++ scripts/weaponDrop.lua
@@ -7,11 +7,11 @@ local weaponDrop = {}
 
 function weaponDrop.new()
     local w = {
-		position = vec2.new();
-		scale = 1;
-		weapon = nil;
-		beingObtained = false;
-		nearPlayer = false;
+	position = vec2.new();
+	scale = 1;
+	weapon = nil;
+	beingObtained = false;
+	nearPlayer = false;
         alpha = 1;
     }
 
@@ -29,57 +29,55 @@ function weaponDrop.new()
         if slot then
             -- Add weapon to existing empty slot
             Player.weapons[slot] = w.weapon
-        else
-            -- Replace the current (TODO)
         end
         w.beingObtained  = true
     end
 
     function w.update(delta, i)
-		if not w.weapon then return end
-		local distance = utils.distanceTo(w.position, Player.position)
-		if w.beingObtained then
-            -- Move towards player
-            local p = Player.position
+	if not w.weapon then return end
+	    local distance = utils.distanceTo(w.position, Player.position)
+	    if w.beingObtained then
+		-- Move towards player
+		local p = Player.position
 	        w.rotation = math.atan2(p.y - w.position.y, p.x - w.position.x)
-            local speed = distance / 0.12
-            w.position.x = w.position.x + math.cos(w.rotation) * (speed * delta)
-            w.position.y = w.position.y + math.sin(w.rotation) * (speed * delta)
-		    w.alpha = distance / 65
-            -- Despawn if near player
-            if distance < 15 then
-                table.remove(WeaponDrops, i) end
-        else
-			local distance = utils.distanceTo(w.position, Player.position)
-			w.nearPlayer = distance < 63
-			-- Change scale
-			if w.nearPlayer then
-				w.scale = w.scale + (1.45-w.scale) / (250 * delta)
+		local speed = distance / 0.12
+		w.position.x = w.position.x + math.cos(w.rotation) * (speed * delta)
+		w.position.y = w.position.y + math.sin(w.rotation) * (speed * delta)
+		w.alpha = distance / 65
+		-- Despawn if near player
+		if distance < 15 then
+		    table.remove(WeaponDrops, i) end
+	    else
+		local distance = utils.distanceTo(w.position, Player.position)
+		w.nearPlayer = distance < 63
+		-- Change scale
+		if w.nearPlayer then
+		    w.scale = w.scale + (1.45-w.scale) / (250 * delta)
                 -- Check for key press
                 if love.keyboard.isDown("e") then
                     w.obtain() end
-            else
-				w.scale = w.scale + (1-w.scale) / (250 * delta)
-            end
+		else
+		    w.scale = w.scale + (1-w.scale) / (250 * delta)
 		end
+	    end
     end
 
     function w.draw()
-		if not w.weapon then return end
-		local image = assets.weapons[w.weapon.name .. "Img"]
-		local width = image:getWidth() ; local height = image:getHeight()
-		local x = (w.position.x - Camera.position.x) * Camera.zoom
-		local y = (w.position.y - Camera.position.y) * Camera.zoom
-		love.graphics.setColor(1, 1, 1, w.alpha)
+	if not w.weapon then return end
+	local image = assets.weapons[w.weapon.name .. "Img"]
+	local width = image:getWidth() ; local height = image:getHeight()
+	local x = (w.position.x - Camera.position.x) * Camera.zoom
+	local y = (w.position.y - Camera.position.y) * Camera.zoom
+	love.graphics.setColor(1, 1, 1, w.alpha)
         love.graphics.draw(
-			image, x, y, 0,
-			1.45*w.scale, 1.45*w.scale, width/2, height/2
-		)
+	    image, x, y, 0,
+	    1.45*w.scale, 1.45*w.scale, width/2, height/2
+	)
         love.graphics.setColor(1, 1, 1, 1)
-		-- Draw preview text
-		if not w.nearPlayer or w.beingObtained then return end
-		love.graphics.setNewFont("fonts/Minecraftia-Regular.ttf", 14)
-		love.graphics.print(string.upper(w.weapon.name), x-width, y-height/2-25)
+	-- Draw preview text
+	if not w.nearPlayer or w.beingObtained then return end
+	love.graphics.setNewFont("fonts/Minecraftia-Regular.ttf", 14)
+	love.graphics.print(string.upper(w.weapon.name), x-width, y-height/2-25)
     end
 
     return w
